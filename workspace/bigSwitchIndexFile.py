ee899169460cc8e26186ef9ad35267083d498afb
David <david@davidpineiro.xyz>
2025-09-12 16:08:08 -0400

    finished bigSwitchIndexFile.py

--- workspace/bigSwitchIndexFile.py
+++ workspace/bigSwitchIndexFile.py
@@ -5,7 +5,7 @@ import sys
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", help="big switch index file path", required=True)
-parser.add_argument("-n" ,help="number of lines to grab from app.decomp", required=True)
+parser.add_argument("-n" ,help="number of lines to grab from app.decomp", default=10, type=int)
 parser.add_argument("-d", help="app.decomp file path", required=True)
 parser.add_argument("-c" ,help="app.section.code file path", required=True)
 # parser.add_argument("-l" ,help="loop label above branch locator")
@@ -87,17 +87,73 @@ with open(args.d) as f:
             # br_table[B_cb, B_kh, B_... B_av, B_of, ..L_b](
             index_branch_mapping = line[line.find("[")+1:line.find("]")].replace("..","").split(",")
 
+            for i in range(len(index_branch_mapping)):
+                index_branch_mapping[i] = index_branch_mapping[i].strip()
+
             branch_table_pos = f.tell()
             break
 
         line = f.readline()
 # print(index_branch_mapping[0])
 
-# loop L_b {
-# label B_wea:
+# indexes = [747, 123, 0]
+indexes = []
+with open(args.i) as f:
+    line = f.readline()
+    while line:
+        line = line.strip()
+        if line.isnumeric():
+            indexes.append(int(line))
 
-indexes = [747, 123, 0]
-# TODO: grab this from args.i file
+        line = f.readline()
 
 
+indexes_result = {}
+with open(args.d) as f:
+    f.seek(large_func_pos)
+    f.readline() # skip the 'function f_ts(a:int) {' part
+    line = f.readline()
+    while line and f.tell() <= large_func_end_pos:
+        for index in indexes:
+            branch_word = index_branch_mapping[index]
+            branch_label_code = f"label {branch_word}:"
+            loop_label_code = f"loop {branch_word} {{"
+            # print(branch_label_code)
+            if branch_label_code in line or loop_label_code in line:
+                # print("in")
+                indexes_result[index] = ""
+                temp = f.tell()
+                i = 0
+                long_count = 0
+                start_spacing = len(line) - len(line.lstrip())
+                while i < args.n and long_count < 123: # 123 inner lines why not
+                    line_b = f.readline()
+                    spacing = len(line_b) - len(line_b.lstrip())
 
+                    if spacing <= start_spacing or f.tell() <= branch_table_pos:
+                        if len(line_b) > 100:
+                            line_b = line_b[:100] + "...;"
+
+                            if line_b.strip().startswith("br_table"):
+                                i = args.n
+
+                        if line_b.strip().startswith("continue ") and line_b.strip().endswith(";"):
+                            i = args.n
+                        indexes_result[index] = indexes_result[index] + " " + line_b.replace("\n","").replace("\t","").strip()
+                        i = i + 1
+                    else:
+                        if len(line_b) > 100:
+                            line_b = line_b[:100] + "...;" + "\n"
+                        indexes_result[index] = indexes_result[index] + line_b
+                        long_count = long_count + 1
+                f.seek(temp)
+
+        line = f.readline()
+
+for index in indexes:
+    if index in indexes_result.keys():
+        print(f"{index} # {indexes_result[index]}")
+    else:
+        print(f"{index} # could not find this one :P")
+
+# print(len(indexes))
