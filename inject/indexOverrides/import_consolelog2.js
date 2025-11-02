8141995d3c1afe851dd193ad0110e5a937876b74
David <david@davidpineiro.xyz>
2025-11-02 06:48:14 -0500

    removed what the sigma

--- inject/indexOverrides/import_consolelog2.js
+++ inject/indexOverrides/import_consolelog2.js
@@ -1,7 +1,7 @@
 (e, t, a, o) => {
     var thing = d.decode(r().subarray(e, e + t));
     if(thing.includes("Stop") || thing.includes("trick")){
-        console.log("WHAT THE SIGMA");
+        // console.log("WHAT THE SIGMA");
         return;
     }
     console.log(
